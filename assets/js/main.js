05bc2f2b4ee7425515a35f13d8b68ef55488ee82
LiamTran1509 <trankhacquyet1996@gmail.com>
2021-08-24 11:26:15 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,157 @@
+// Một số bài hát có thể bị lỗi do liên kết bị hỏng. Vui lòng thay thế liên kết khác để có thể phát
+// Some songs may be faulty due to broken links. Please replace another link so that it can be played
+
+
+
+// Some things that I have to do here
+// 1. Render song
+// 2. Scroll to hide and appear
+// 3. play the song
+$$ = document.querySelectorAll.bind(document);
+const $ = document.querySelector.bind(document);
+const PLAYER_KEY = 'F8-player';
+const heading = $('header h2');
+const cdThumb = $('.cd-thumb');
+const audio = $('#audio');
+const player = $('.player');
+const btnPlay = $('.btn-toggle-play');
+const cd = $('.cd');
+const progress = $('#progress');
+const btnPrev = $('.btn-prev');
+const btnRepeat = $('.btn-repeat');
+const btnNext = $('.btn-next');
+const btnRandom = $('.btn-random');
+const playlist = $('.playlist');
+// console.log(cd.offsetWidth)
+
+const PLAYER_STORAGE_KEY = 'QUYET_PLAYER'
+
+const app = {
+    currentIndex: 0,
+    songs: [{
+            name: 'Nevada',
+            singer: 'Vicetone',
+            path: 'https://aredir.nixcdn.com/NhacCuaTui924/Nevada-Vicetone-4494556.mp3?st=_IjpS9u0LjapNgzm058wVw&e=1623143773',
+            image: 'https://i.pinimg.com/originals/f8/6f/33/f86f3378e656883b33594f06d78d1634.jpg',
+        },
+        {
+            name: 'Light It Up',
+            singer: 'Robin Hustin x TobiMorrow',
+            path: 'https://aredir.nixcdn.com/NhacCuaTui968/LightItUp-RobinHustinTobimorrowJex-5619031.mp3?st=kzpVQ5kKnf2LlcAqM6lnxg&e=1623143881',
+            image: 'https://avatar-ex-swe.nixcdn.com/song/2019/01/08/1/3/d/a/1546913843457_640.jpg',
+        },
+        {
+            name: 'Yoru ni kakeru',
+            singer: 'YOASOBI',
+            path: 'https://aredir.nixcdn.com/NhacCuaTui992/YoruNiKakeru-YOASOBI-6149490.mp3?st=68hnFhtGF6RukKDcDcW9Mw&e=1623132179',
+            image: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/16788ee5-3436-474a-84fd-6616063a1a9a/de2f4eq-bc67fa17-8dae-46a9-b85d-fe8082c34841.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzE2Nzg4ZWU1LTM0MzYtNDc0YS04NGZkLTY2MTYwNjNhMWE5YVwvZGUyZjRlcS1iYzY3ZmExNy04ZGFlLTQ2YTktYjg1ZC1mZTgwODJjMzQ4NDEucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.dABuqANeQEs6FBfslZHdG1lW_gDwzf61yqiSABROSx0',
+        },
+        {
+            name: 'Muộn rồi mà sao còn',
+            singer: 'Sơn Tùng M-TP',
+            path: 'https://aredir.nixcdn.com/Believe_Audio19/MuonRoiMaSaoCon-SonTungMTP-7011803.mp3?st=w9AA-eyRI7yD_VYGfvVWeQ&e=1623141624',
+            image: 'https://pbs.twimg.com/media/Ez5jRyVVgAQN6Kh.jpg',
+        },
+        {
+            name: 'See You Again',
+            singer: 'Charlie Puth ft Wiz Khalifa',
+            path: 'https://aredir.nixcdn.com/NhacCuaTui894/SeeYouAgain-KurtSchneiderEppicAlexGoot-3888930.mp3?st=1q73myBS8FKr8Rx0snpMJw&e=1623144094',
+            image: 'https://nghiennhac.com/wp-content/uploads/2020/09/see-you-again-0.jpg',
+        },
+
+        {
+            name: 'Symphony',
+            singer: 'Clean Bandit',
+            path: 'https://aredir.nixcdn.com/Sony_Audio37/Symphony-CleanBanditZaraLarsson-4822950.mp3?st=sPgJSXtRXYpT_rznXyez6g&e=1623144426',
+            image: 'https://i.ytimg.com/vi/PIf9GvWaxQQ/maxresdefault.jpg',
+        },
+        {
+            name: 'Waiting For Love',
+            singer: 'Avicii',
+            path: 'https://aredir.nixcdn.com/Unv_Audio45/WaitingForLove-Avicii-4203283.mp3?st=mXGv6kIqbxg_coAyUqzlnw&e=1623144462',
+            image: 'https://i.ytimg.com/vi/Hmbm3G-Q444/maxresdefault.jpg',
+        },
+        {
+            name: 'Alone',
+            singer: 'Marshmello',
+            path: 'https://aredir.nixcdn.com/NhacCuaTui927/Alone-Marshmello-4456939.mp3?st=RTsMC9tNcKEi8fd0iKtdaA&e=1623144502',
+            image: 'https://i.ytimg.com/vi/UNB8F0ObA4g/maxresdefault.jpg',
+        },
+        {
+            name: 'Something Just Like This',
+            singer: 'The Chainsmokers & Coldplay',
+            path: 'https://aredir.nixcdn.com/Sony_Audio39/SomethingJustLikeThis-TheChainsmokersColdplay-5337136.mp3?st=VQuH6VgNsPlBizbk-c7n3w&e=1623144556',
+            image: 'https://avatar-ex-swe.nixcdn.com/song/2017/11/07/a/1/4/5/1510038809679_640.jpg',
+        },
+        {
+            name: 'Sugar',
+            singer: 'Maroon 5',
+            path: 'https://aredir.nixcdn.com/Unv_Audio73/Sugar-Maroon5-3338455.mp3?st=3FUWEyikJePPeAuREUcw9g&e=1623144644',
+            image: 'https://i.ytimg.com/vi/7vw84EkHOlY/maxresdefault.jpg',
+        },
+    ],
+
+    render: function() {
+        const htmls = this.songs.map((song, index) => {
+            // console.log("🚀 ~ file: main.js ~ line 95 ~ htmls ~ index", index)
+            // console.log("🚀 ~ file: main.js ~ line 95 ~ htmls ~ song", song)
+            return `
+             <div data-index="${index}" class="song ${index === this.currentIndex ? 'active' : ''}">
+                <div class="thumb" style="background-image: url('${song.image}');"></div>
+                <div class="body">
+                    <h3 class="title">${song.name}</h3>
+                    <p class="author">${song.singer}</p>
+                </div>
+                <div class="option">
+                <i class="fas fa-ellipsis-h"></i>
+                </div>
+            </div>`
+
+
+        });
+        playlist.innerHTML = htmls.join('')
+    },
+    defineProperty: function() {
+        Object.defineProperty(this, 'currentSong', {
+            get: function() {
+                return this.songs[this.currentIndex];
+            },
+        });
+    },
+    loadCurrentSong: function() {
+        heading.textContent = this.currentSong.name;
+        cdThumb.style.backgroundImage = `url('${this.currentSong.image}')`;
+        audio.src = this.currentSong.path;
+    },
+    hanldeEvents: function() {
+        const cdWidth = cd.offsetWidth
+        const cdThumbAnimation = cdThumb.animate(
+            [{
+                transform: 'rotate(360deg)'
+            }, ], {
+                duration: 10000,
+                interations: Infinity
+            }
+        )
+        cdThumbAnimation.pause()
+            //scroll and hide cd thumb
+        document.onscroll = function() {
+            const scrollWidth = document.documentElement.scrollTop || window.scrollY
+                // console.log("🚀 ~ file: main.js ~ line 140 ~ scrollWidth", scrollWidth)
+            const newWidth = cdWidth - scrollWidth
+                // console.log("🚀 ~ file: main.js ~ line 142 ~ newWidth", newWidth)
+            cd.style.width = newWidth > 0 ? newWidth + 'px' : 0
+            cd.style.opacity = newWidth / cdWidth
+        }
+
+    },
+    start: function() {
+        this.defineProperty()
+        this.loadCurrentSong();
+        this.render();
+        this.hanldeEvents()
+
+    },
+
+};
+app.start()
